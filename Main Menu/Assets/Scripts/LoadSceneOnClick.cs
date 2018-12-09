c90e7f77ad095706e1c75b9d529edd9ae6b68beb
Jonathan Way <jonathan.way@loop.colum.edu>
2018-12-09 15:52:30 -0600

    Funtionality
    - Renamed main menu buttons to make sense
    - added an extra scene to the buid settins for when start is clicked
    - made the start button switch to a new scene
    - made the help button switch to the help menu and made its back button switch back to the main menu

---
+++ Main Menu/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    // Use this for initialization
+    public void LoadSceneOnIndex(int screenIndex)
+    {
+        SceneManager.LoadScene(screenIndex);
+    }
+}
